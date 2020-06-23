694870997e22525f18a4473eaa8d274f70a5a105
Edmund Dipple <elmundio1987@gmail.com>
2020-06-23 14:32:28 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+LATEST_VERSION=`curl -L https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r ."latest"."release"`
+echo "##vso[build.updatebuildnumber]${LATEST_VERSION}"
+
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar --rev $LATEST_VERSION -o output
+cp output/spigot-${LATEST_VERSION}.jar output/spigot-release.jar 
